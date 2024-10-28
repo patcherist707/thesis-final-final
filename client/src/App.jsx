54da33fe4382c2bef84a11607af8a4286133aa01
FALabyow <ruhaynaadje08@gmail.com>
2024-10-29 02:37:46 +0800

    Creates routes and pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,23 @@
-import { Button } from "flowbite-react";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Dashboard from "./pages/Dashboard";
+import Feedback from "./pages/Feedback";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
 
 
 export default function App() {
   return (
-    <div>
-      <Button>Click me</Button>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/dashboard" element={<Dashboard/>}/>
+        <Route path="/feedback" element={<Feedback/>}/>
+        <Route path="/sign-in" element={<SignIn/>}/>
+        <Route path="/sign-up" element={<SignUp/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
