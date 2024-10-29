fda35dd38d338b20b691fb8a151dd8f7e46dbbd8
FALabyow <ruhaynaadje08@gmail.com>
2024-10-30 00:09:50 +0800

    Firebase connection to a server

--- api/index.js
+++ api/index.js
@@ -1,10 +1,17 @@
 import express from "express";
+import dotenv from "dotenv";
+import testRoutes from "./test-folder/test.route.js";
+import {createServer} from 'node:http';
 
-
+dotenv.config();
 const app = express();
 const port = 3000;
+const httpServer = createServer(app);
+
+app.use(express.json());
+app.use('/api', testRoutes);
 
 
-app.listen(port, () => {
+httpServer.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
